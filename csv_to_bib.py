f730430671df6f0740c2b57ccebf7c77af80e6dc
Alexandre Augusto <alexandrebarbaruiva@users.noreply.github.com>
2018-03-05 13:02:54 -0300

    Update csv_to_bib.py

--- csv_to_bib.py
+++ csv_to_bib.py
@@ -5,6 +5,7 @@ from os.path import isfile, join, dirname, realpath
 from time import sleep
 from os import remove
 
+
 def find_all_files(mypath = dirname(realpath(__file__))):
     """
     Function that returns a list with the names of all the file_type files
@@ -19,34 +20,44 @@ def find_all_files(mypath = dirname(realpath(__file__))):
     return(chosen_files)
 
 def read_files(list_of_files):
-    # print(list_of_files)
     all_data = []
     all_names = []
+    print("Hi1")
     for arquivo in range(len(list_of_files)):
-        filename=list_of_files[arquivo]
+        filename = list_of_files[arquivo]
         dataArray = []
         count = 0
+        print("Hi2")
         with open(filename,'r') as csvfile:
-            reader = csv.reader(csvfile,delimiter=';')
-            iskey=True
+            print("Hi3")
+            """
+            When first line of CSV is composed of keys has_key must be true
+            """
+            has_key = iskey = True
             keys = []
+            reader = csv.reader(csvfile,delimiter=';')
+            print("Hi31")
             for row in reader:
                 if iskey:
+                    print("Hi33")
                     keys = row
-                    iskey=False
+                    iskey = False
                     keys = [k for k in keys if k!='']
                 else:
-                    dictAux = {}
+                    clean_dataframe = {}
                     rowArr = row
                     for i in range(len(keys)):
-                        dictAux[str(keys[i])] = str(rowArr[i])
-                    dictAux["ide"] = count
+                        clean_dataframe[str(keys[i])] = str(rowArr[i])
+                    clean_dataframe["ide"] = count
                     count = count+1
-                    dataArray.append(dictAux);
+                    dataArray.append(clean_dataframe)
+            print("HiFILE")
         list_of_files[arquivo] = list_of_files[arquivo][:-4]
         nome_arquivo = list_of_files[arquivo] + ".bib"
         all_data.append(dataArray)
         all_names.append(nome_arquivo)
+        print("Hi4")
+    print("Hi5")
     return([all_data, all_names])
 
 def generate_data(data_array, file_name):
@@ -61,18 +72,18 @@ def generate_data(data_array, file_name):
         try:
             print(file_name)
             for data in data_array:
-                data['Keywords'] = data['Keywords'].replace(',','; ').replace(', ', '; ')
-                to_print += ('\n@ARTICLE{mctic'+str(data['ide'])+',\n')
-                to_print += ('author={'+data['Tipo']+'},\n')
-                to_print += ('title={'+data['Instituto']+'},\n')
-                to_print += ('journal={'+data['Sigla']+'},\n')
-                to_print += ('year={2017},\n')
-                to_print += ('doi={'+str(data['ide'])+'},\n')
-                to_print += ('abstract={'+data['Documento']+'},\n')
-                to_print += ('language={Brazilian Portuguese},\n')
-                to_print += ('document_type={Article},\n')
-                to_print += ('keywords={' + data['Keywords'] + '},\n')
-                to_print += ('source={Scopus},\n}\n')
+                # data['Keywords'] = data['Keywords'].replace(',','; ').replace(', ', '; ')
+                # to_print += ('\n@ARTICLE{mctic'+str(data['ide'])+',\n')
+                # to_print += ('author={'+data['Tipo']+'},\n')
+                # to_print += ('title={'+data['Instituto']+'},\n')
+                # to_print += ('journal={'+data['Sigla']+'},\n')
+                # to_print += ('year={2017},\n')
+                # to_print += ('doi={'+str(data['ide'])+'},\n')
+                # to_print += ('abstract={'+data['Documento']+'},\n')
+                # to_print += ('language={Brazilian Portuguese},\n')
+                # to_print += ('document_type={Article},\n')
+                # to_print += ('keywords={' + data['Keywords'] + '},\n')
+                # to_print += ('source={Scopus},\n}\n')
         except Exception as e:
             print("Error: " + str(e))
             with open("error.txt","w") as erros:
@@ -86,6 +97,7 @@ def generate_data(data_array, file_name):
 
 if __name__ == "__main__":
     files = find_all_files()
+    print(files)
     dados, nome_arquivos = read_files(files)
     for posicao in range(len(dados)):
         generate_data(dados[posicao], nome_arquivos[posicao])
